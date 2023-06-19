ca4d4c1a665b1f56c136b4635fd2a648bbdb88e9
Niklas Lausch <niki@lauschig.net>
2023-06-19 20:50:15 +0200

    Cleanup - Removed old fresh content - Changed to last id txt file

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # KommtKevinOnline.de
 
-[![Made with Fresh](https://fresh.deno.dev/fresh-badge-dark.svg)](https://fresh.deno.dev)
-
 ### Usage
 
 Start the project:
--- main.ts
+++ main.ts
@@ -1,22 +1,8 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
+import "https://deno.land/std@0.192.0/dotenv/load.ts";
 
-import "$std/dotenv/load.ts";
+import { Cron } from "https://deno.land/x/croner@6.0.3/dist/croner.js";
+import { PollNewVods } from "./src/CheckVods.ts";
 
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
-
-// import { Cron } from "https://deno.land/x/croner@6.0.3/dist/croner.js";
-// import { PollNewVods } from "./src/CheckVods.ts";
-
-// new Cron("0 0 * * * *", () => { PollNewVods('50985620') });
+new Cron("0 0 * * * *", () => { PollNewVods('50985620') });
 
 // PollNewVods('50985620')
-
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
