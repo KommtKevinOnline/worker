f026e42f82d72cf2105129645c92edcf9e272d37
Niklas Lausch <niki@lauschig.net>
2025-04-18 13:38:26 +0200

    feat: upgrade to go 1.24 feat: use cheaper / better model

--- chatgpt.go
+++ chatgpt.go
@@ -12,7 +12,6 @@ import (
 	"github.com/sashabaranov/go-openai/jsonschema"
 )
 
-
 type Upcoming struct {
 	Dates []string `json:"dates"`
 }
@@ -21,36 +20,36 @@ func classify(transcription string, video api.Video) (Upcoming, error) {
 	client := openai.NewClient(os.Getenv(("OPENAI_CHATGPT_TOKEN")))
 
 	functionDefinitions := openai.FunctionDefinition{
-  Name: "has_online_intend",
-	Description: "Evaluate the text that had been said by a twitch streamer at the end of his livestream. Try to find out based on that text, if the streamer is going to stream again the following days.",
-  Parameters: jsonschema.Definition{
-    Type: jsonschema.Object,
-		Properties: map[string]jsonschema.Definition{
-			"dates": {
-				Type: jsonschema.Array,
-				Items: &jsonschema.Definition{
-					Type: jsonschema.String,
+		Name:        "has_online_intend",
+		Description: "Evaluate the text that had been said by a twitch streamer at the end of his livestream. Try to find out based on that text, if the streamer is going to stream again the following days.",
+		Parameters: jsonschema.Definition{
+			Type: jsonschema.Object,
+			Properties: map[string]jsonschema.Definition{
+				"dates": {
+					Type: jsonschema.Array,
+					Items: &jsonschema.Definition{
+						Type: jsonschema.String,
+					},
+					Description: "The array of dates he plan's to stream again in RFC3339 format.",
 				},
-				Description: "The array of dates he plan's to stream again in RFC3339 format.",
 			},
 		},
-	},
-}
+	}
 
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model: openai.GPT4,
+			Model:     openai.GPT4oMini,
 			Functions: []openai.FunctionDefinition{functionDefinitions},
 			Messages: []openai.ChatCompletionMessage{
 				{
-					Role:		openai.ChatMessageRoleSystem,
+					Role:    openai.ChatMessageRoleSystem,
 					Content: "You are an assistant that trys to evaluate if and when a twitch livestreamer will stream again based on a text that had been said during a past livestream. If you are unsure about the time, default to 16:30. If he doesn't say something if he is planning to stream or not, default to an empty array.",
 				},
 				{
-					Role:    openai.ChatMessageRoleUser,
+					Role: openai.ChatMessageRoleUser,
 					Content: fmt.Sprintf(
-					"The date of the transcription is is %s. text: %s",
+						"The date of the transcription is is %s. text: %s",
 						video.PublishedAt.Format(time.RFC3339),
 						transcription,
 					),
--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.21
 require (
 	github.com/Adeithe/go-twitch v0.2.3
 	github.com/joho/godotenv v1.5.1
+	github.com/u2takey/ffmpeg-go v0.5.0
 	golang.org/x/oauth2 v0.15.0
 )
 
 require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
 )
